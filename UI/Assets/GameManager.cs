150684c20296e05b9cb5df27dd0c4d89178d6b66
F-P-P <55227308+F-P-P@users.noreply.github.com>
2019-10-24 14:51:39 +0800

    20191024

---
+++ UI/Assets/GameManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+public class GameManager : MonoBehaviour{
+
+   public AudioMixer mixer;
+
+   public void SetVBGM(float value)
+    {
+        mixer.SetFloat("VBGM", value);
+    }
+
+}
