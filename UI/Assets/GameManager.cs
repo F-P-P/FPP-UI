7edce677e4edbc4f32bc7de417b5b74ff506d0c8
F.P.P <kelly1999010212@gmail.com>
2019-10-31 14:45:35 +0800

    20191031-1

--- UI/Assets/GameManager.cs
+++ UI/Assets/GameManager.cs
@@ -1,13 +1,22 @@
 ﻿using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour{
 
    public AudioMixer mixer;
 
+   public Text loadingText;
+   public Slider loading;
+
    public void SetVBGM(float value)
     {
         mixer.SetFloat("VBGM", value);
     }
+    public void SetVSFX(float value)
+    {
+        mixer.SetFloat("VSFX", value);
+    }
+    
 
 }
