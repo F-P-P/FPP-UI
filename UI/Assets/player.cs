5971d99913b6354bb4d072b7d50b7ba8baa1979d
FPP <kelly1999010212@gmail.com>
2019-12-12 14:54:27 +0800

    20191212

---
+++ UI/Assets/player.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+
+public class player : MonoBehaviour
+{
+    public int hp = 100;
+    public Slider hpSlider;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        if (other.tag == "trap")
+        {
+            int d = other.GetComponent<trap>().damage;
+            hp -= d;
+            hpSlider.value = hp;
+        }
+    }
+    private void OnParticleCollision(GameObject other)
+    {
+        if (other.tag == "trap")
+        {
+            int d = other.GetComponent<trap>().damage;
+            hp -= d;
+            hpSlider.value = hp;
+        }
+    }
+}
